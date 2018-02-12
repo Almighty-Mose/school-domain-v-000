88cd5b38412f2450d7df0dfb4538d664b1ebf36a
Branden Mosley <branden.mosley1337@gmail.com>
2018-02-12 21:02:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_accessor :name
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def roster=(roster)
+    @roster = roster
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+      @roster[grade] = []
+      @roster[grade] << student
+  end
+end
